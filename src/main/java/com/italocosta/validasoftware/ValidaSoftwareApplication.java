6cb98db2bfad94750231fa9476a497459fdfa371
Italo Costa <italo.costa.ce@gmail.com>
2017-01-15 02:00:01 -0300

    Closes issue #1

---
+++ src/main/java/com/italocosta/validasoftware/ValidaSoftwareApplication.java
@@ -0,0 +1,12 @@
+package com.italocosta.validasoftware;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ValidaSoftwareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ValidaSoftwareApplication.class, args);
+	}
+}
---
+++ src/test/java/com/italocosta/validasoftware/ValidaSoftwareApplicationTests.java
@@ -0,0 +1,16 @@
+package com.italocosta.validasoftware;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ValidaSoftwareApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
